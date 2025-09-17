2497fcabd8e608f871bdddab674ad3d34a967a9b
kiran-ab01 <132429361+kiran-ab01@users.noreply.github.com>
2025-09-17 19:12:07 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
 provider "aws" {
-  region = var.aws_region
+  region = eu-north-1
 }
 
 resource "aws_instance" "test" {
-  ami           = "ami-0c1a7f89451184c8b" # Amazon Linux 2 (ap-south-1, update for your region)
-  instance_type = "t2.micro"
+  ami           = "ami-043339ea831b48099" # Amazon Linux 2 (ap-south-1, update for your region)
+  instance_type = "t3.micro"
 
   tags = {
     Name = "Octopus-Test-EC2"
