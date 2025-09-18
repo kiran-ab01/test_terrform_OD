fe353ecd48b706de3360ad40bbc61a26756e7050
kiran-ab01 <132429361+kiran-ab01@users.noreply.github.com>
2025-09-18 10:39:28 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 provider "aws" {
-  region = eu-north-1
+  region = us-east-1
 }
 
 resource "aws_instance" "test" {
-  ami           = "ami-043339ea831b48099" # Amazon Linux 2 (ap-south-1, update for your region)
+  ami           = "ami-08982f1c5bf93d976" # Amazon Linux 2 (ap-south-1, update for your region)
   instance_type = "t3.micro"
 
   tags = {
