9fd5ccda2ca023cfd1efb599bb7e23d8ccd65126
kiran-ab01 <132429361+kiran-ab01@users.noreply.github.com>
2025-09-18 10:18:53 +0530

    Update my.sh

--- my.sh
+++ my.sh
@@ -1,4 +1,10 @@
+
+export AWS_ACCESS_KEY_ID=#AKIATJL7ZOJZUCRWKGOQ
+export AWS_SECRET_ACCESS_KEY=#0XHyLbszmltbr+yAgg06ZrG+iseCApyEZ2Q+NggF
+export AWS_DEFAULT_REGION=#us-east-1
+
 export DOTNET_SYSTEM_GLOBALIZATION_INVARIANT=true
+
 terraform init
 terraform plan -out=tfplan
 terraform apply -auto-approve tfplan
